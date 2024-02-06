560d4cb43d79eb192488da7a6c863ff9608b24ad
Artan Vrajolli <artanvrajolli@gmail.com>
2024-02-06 12:48:23 +0100

    committed triggered via commit.bat file

---
+++ commit.bat
@@ -0,0 +1,18 @@
+@echo off
+
+REM Run npm build
+call npm run build
+
+REM Replace {{script}} with 'https://example.com' in README.MD
+set "replacement=https://example.com"
+set "file=README.MD"
+call powershell -Command "(gc '%file%') -replace '{{script}}', '%replacement%' | Out-File -encoding ASCII '%file%'"
+
+REM Add all files to the git repository
+call git add .
+
+REM Commit the changes
+call git commit -m "committed triggered via commit.bat file"
+
+REM Push the changes to the remote repository
+call git push -uf origin main
