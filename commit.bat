2622cd0a0bcff52354f746b0fdedb9fdfbf4b4d8
Artan Vrajolli <artanvrajolli@gmail.com>
2024-02-12 16:55:16 +0100

    AA

--- commit.bat
+++ commit.bat
@@ -4,7 +4,7 @@ REM Run npm build
 call npm run build
 
 REM Replace {{script}} with 'https://example.com' in README.MD
-set "replacement=https://artanvrajolli.github.io/publicstuff/assets/index.js"
+set "replacement=https://artanvrajolli.github.io/publicstuff/assets/index.js?x=2"
 set "file=README.MD"
 call powershell -Command "(gc README-TEMPLATE.MD) -replace '{{script}}', '%replacement%' | Out-File -encoding ASCII '%file%'"
 
