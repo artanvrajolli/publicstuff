531ff26ed48dfc06e101c868ec047c9085f0c823
Artan Vrajolli <artanvrajolli@gmail.com>
2024-02-16 11:08:00 +0100

    committed triggered via commit.bat file

--- commit.bat
+++ commit.bat
@@ -4,7 +4,13 @@ REM Run npm build
 call npm run build
 
 REM Replace {{script}} with 'https://example.com' in README.MD
-set "replacement=https://artanvrajolli.github.io/publicstuff/assets/index.js?x=%time%"
+@REM set "replacement=https://artanvrajolli.github.io/publicstuff/assets/index.js?x=%time%"
+
+@echo off
+for /f %%x in ('wmic path win32_utctime get /format:list ^| findstr "="') do set %%x
+set /a "ticks=(%Year% - 1970) * 31536000 + (%Month% - 1) * 2592000 + (%Day% - 1) * 86400 + %Hour% * 3600 + %Minute% * 60 + %Second%"
+set "replacement=https://artanvrajolli.github.io/publicstuff/assets/index.js?x=%ticks%"
+
 
 set "file=README.MD"
 call powershell -Command "(gc README-TEMPLATE.MD) -replace '{{script}}', '%replacement%' | Out-File -encoding ASCII '%file%'"
