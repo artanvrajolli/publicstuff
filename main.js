1030b77275c75901d23330b01948e374142a03de
Artan Vrajolli <artanvrajolli@gmail.com>
2024-02-16 10:29:32 +0100

    committed triggered via commit.bat file

--- main.js
+++ main.js
@@ -42,7 +42,11 @@ class MenuKorePlus {
     cookies = {};
     platformsList = [];
     servicesList = [];
+
+    currentWebsite = null;
+    
     constructor(platformsList,servicesList) {
+        this.currentWebsite = window.location.hostname;
         this.platformsList = platformsList;
         this.servicesList = servicesList;
         this.constructorInitAsync();
@@ -136,7 +140,7 @@ class MenuKorePlus {
             return this.componentMenu({
                 title: item.title,
                 img: item.img,
-                shortDesc: item.shortDesc
+                shortDesc: item.shortDesc,
             });
         });
     }
@@ -267,6 +271,9 @@ class MenuKorePlus {
         }
 
         divCont.classList.add(this.cleanClass('main-menu-plus-offset-c1'));
+        if(this.currentWebsite.toLowerCase() === text.toLowerCase() || text.includes('.')){
+            divCont.classList.add(this.cleanClass('active'));
+        }
         let styles = ops?.style || {};
         for (const [key, value] of Object.entries(styles)) {
             divCont.style[key] = value;
